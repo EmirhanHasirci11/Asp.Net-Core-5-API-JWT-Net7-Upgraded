634b96699fd3fc0f7f72c63559f69431cbaf9978
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-03-29 13:21:44 +0300

    Lesson 9

---
+++ UdemyAuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAuthServer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAllAsync();
+        IQueryable<T> Where(Expression<Func<T,bool>>expression);
+
+        Task AddAsync(T entity);
+        void Remove(T entity);
+        T Update(T entity);
+
+    }
+}
