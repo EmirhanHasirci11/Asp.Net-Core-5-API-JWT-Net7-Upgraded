5c1ac94f443a5d42fced62a2a476e3d3e7d16860
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-03-29 13:03:00 +0300

    Lesson 8

---
+++ UdemyAuthServer.Core/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAuthServer.Core.Entities
+{
+    public class AppUser:IdentityUser
+    {
+        public string City{ get; set; }
+        
+    }
+}
