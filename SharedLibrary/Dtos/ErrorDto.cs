8a0850edf226fe9676506bf563059747791a9f18
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-03-29 13:42:56 +0300

    Lesson 10

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get;private set; }
+        public bool IsShow{ get; set; }
+        public ErrorDto()
+        {
+            Errors = new();
+        }
+        public ErrorDto(string error,bool isShow)
+        {
+            Errors=new();
+            Errors.Add(error);
+            IsShow=isShow;
+        }
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = new();
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+}
