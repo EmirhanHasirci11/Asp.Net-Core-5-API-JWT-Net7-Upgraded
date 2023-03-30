4ecd4b8b24843955b32e3e6f253ecb28342e2d0b
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-03-30 18:52:07 +0300

    Lesson 11

---
+++ SharedLibrary/Dtos/CustomResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class CustomResponse<T> where T : class
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; }
+        public ErrorDto Error { get;private set; }
+        [JsonIgnore]
+        public bool IsSuccessful { get;private set; }
+        public static CustomResponse<T> Success(T data, int statusCode)
+        {
+            return new CustomResponse<T>() { Data = data, StatusCode = statusCode,IsSuccessful=true };
+        }
+        public static CustomResponse<T> Success(int statusCode)
+        {
+            return new CustomResponse<T>() { Data = default, StatusCode = statusCode,IsSuccessful = true };
+        }
+        public static CustomResponse<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new CustomResponse<T>() { Error = errorDto, StatusCode = statusCode,IsSuccessful=false };
+        }
+        public static CustomResponse<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new CustomResponse<T>() { Error = errorDto, StatusCode = statusCode,IsSuccessful=false };
+        }
+
+        
+    }
+}
