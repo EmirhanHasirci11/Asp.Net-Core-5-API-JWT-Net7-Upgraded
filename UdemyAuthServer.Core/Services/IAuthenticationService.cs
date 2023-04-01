34e23d06ce8f2a0475ade88fc22a18b6bafa27f3
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-04-01 18:45:20 +0300

    Lesson 15

---
+++ UdemyAuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,19 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Dtos;
+
+namespace UdemyAuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<CustomResponse<TokenDto>> CreateToken(LoginDto loginDto);
+        Task<CustomResponse<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<CustomResponse<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        Task<CustomResponse<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
