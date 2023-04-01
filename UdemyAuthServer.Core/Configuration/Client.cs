6b77bf1467f37556445e502782430e15da930674
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-04-01 18:20:39 +0300

    Lesson 14

---
+++ UdemyAuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public string Id{ get; set; }
+        public string Secret{ get; set; }
+        public List<String> Audiences { get; set; }
+    }
+}
---
+++ UdemyAuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Configuration;
+using UdemyAuthServer.Core.Dtos;
+using UdemyAuthServer.Core.Entities;
+
+namespace UdemyAuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(AppUser appUser);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
