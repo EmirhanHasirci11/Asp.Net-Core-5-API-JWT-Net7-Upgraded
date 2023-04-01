e29b5dbfcc632b18dbdb11279d0de370e80fdafa
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-04-01 21:29:37 +0300

    Lesson 18

---
+++ UdemyAuthServer.Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Entities;
+
+namespace UdemyAuthServer.Data
+{
+    public class AppDbContext:IdentityDbContext<AppUser,IdentityRole,string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions):base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens{ get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
