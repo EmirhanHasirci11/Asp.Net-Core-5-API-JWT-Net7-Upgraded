f3f379f6548f52d49611f70bc66eb5677f83031a
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-04-01 19:05:57 +0300

    Lesson 16

---
+++ UdemyAuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Dtos;
+
+namespace UdemyAuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<CustomResponse<AppUserDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<CustomResponse<AppUserDto>> GetUserByNameAsync(string userName);
+    }
+}
