abaf44d39b22fd7fa61605ed8c4c4a8af0eb52e5
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-04-02 14:01:05 +0300

    Lesson 22

---
+++ UdemyAuthServer.Service/DtoMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Dtos;
+using UdemyAuthServer.Core.Entities;
+
+namespace UdemyAuthServer.Service
+{
+    public class DtoMapper:Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto,Product>().ReverseMap();
+            CreateMap<AppUserDto,AppUser>().ReverseMap();
+
+        }
+    }
+}
