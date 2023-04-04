5de7cbb65d832b8ff019386f2eb2ba57c626a8e8
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-04-05 02:20:31 +0300

    Lesson 32

---
+++ UdemyAuthServer.Service/Services/AuthenticationService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Configuration;
+using UdemyAuthServer.Core.Dtos;
+using UdemyAuthServer.Core.Entities;
+using UdemyAuthServer.Core.Repositories;
+using UdemyAuthServer.Core.Services;
+using UdemyAuthServer.Core.UnitOfWork;
+
+namespace UdemyAuthServer.Service.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly List<Client> _clients;
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<UserRefreshToken> _genericService;
+
+        public AuthenticationService(IOptions<List<Client>> optionClient, ITokenService tokenService, UserManager<AppUser> userManager, IUnitOfWork unitOfWork, IGenericRepository<UserRefreshToken> genericService)
+        {
+            _clients = optionClient.Value;
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _genericService = genericService;
+        }
+
+        public Task<CustomResponse<TokenDto>> CreateToken(LoginDto loginDto)
+        {            
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponse<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponse<TokenDto>> CreateTokenByRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponse<NoDataDto>> RevokeRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
