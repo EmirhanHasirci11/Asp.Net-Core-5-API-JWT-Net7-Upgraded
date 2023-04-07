6c6d888f49758e7c9d36239b82a413502d91f26d
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-04-07 21:13:05 +0300

    Lesson 47

---
+++ MiniApp1.API/Controllers/StockController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using System.Linq;
+using System.Security.Claims;
+
+namespace MiniApp1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStock()
+        {
+
+            var userName = HttpContext.User.Identity.Name;
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            var userMail = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Email);
+            return Ok($"StockValues=> Username:{userName} Id:{userId} Mail:{userMail}");
+        }
+
+    }
+}
