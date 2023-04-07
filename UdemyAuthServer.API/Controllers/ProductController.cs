1c579a776392d837dfaaef659f22998ca191b802
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-04-07 18:11:23 +0300

    Lesson 45

--- UdemyAuthServer.API/Controllers/ProductController.cs
+++ UdemyAuthServer.API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using UdemyAuthServer.Core.Dtos;
@@ -7,6 +8,7 @@ using UdemyAuthServer.Core.Services;
 
 namespace UdemyAuthServer.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ProductController : CustomBaseController
