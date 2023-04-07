0583f61007fd529932828f550b0af6e701d55b19
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2023-04-07 17:46:47 +0300

    Lesson 44

---
+++ UdemyAuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Dtos;
+using UdemyAuthServer.Core.Entities;
+using UdemyAuthServer.Core.Services;
+
+namespace UdemyAuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IGenericService<Product,ProductDto> _genericService;
+
+        public ProductController(Core.Services.IGenericService<Product,ProductDto> genericService)
+        {
+            _genericService = genericService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return ActionResultInstance(await _genericService.GetAllAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return ActionResultInstance(await _genericService.GetByIdAsync(id));
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _genericService.AddAsync(productDto));
+        }
+        [HttpPut]
+        public async Task<IActionResult>UpdateProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _genericService.UpdateAsync(productDto,productDto.Id));
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult>DeleteProduct(int id)
+        {
+            return ActionResultInstance(await _genericService.RemoveAsync(id));
+        }
+    }
+}
